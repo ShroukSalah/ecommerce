dd20ce2d6a6275979266cef02c3a67492c15d6c9
shourok_salah <shourok_salah@codebase-tech.com>
2023-05-16 15:01:30 +0300

    auth

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="q-pa-md tw-mx-auto" style="max-width: 400px">
+    <div class=" tw-mx-auto">
         <!-- <div>
             <h3>
                 Don't have an account?
@@ -24,48 +24,50 @@
                 </div>
             </q-form>
         </div> -->
-        <div class="login-page">
-            <transition name="fade">
-                <div v-if="!registerActive" class="wallpaper-login"></div>
-            </transition>
-            <div class="wallpaper-register"></div>
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
-                        <div v-if="!registerActive" class="card login" v-bind:class="{ error: emptyFields }">
-                            <h1>Sign In</h1>
-                            <form class="form-group">
-                                <input v-model="emailLogin" type="email" class="form-control" placeholder="Email" required>
-                                <input v-model="passwordLogin" type="password" class="form-control" placeholder="Password"
-                                    required>
-                                <input type="submit" class="btn btn-primary" @click="doLogin">
-                                <p>Don't have an account? <a href="#"
-                                        @click="registerActive = !registerActive, emptyFields = false">Sign up here</a>
-                                </p>
-                                <p><a href="#">Forgot your password?</a></p>
-                            </form>
-                        </div>
 
-                        <div v-else class="card register" v-bind:class="{ error: emptyFields }">
-                            <h1>Sign Up</h1>
-                            <form class="form-group">
-                                <input v-model="emailReg" type="email" class="form-control" placeholder="Email" required>
-                                <input v-model="passwordReg" type="password" class="form-control" placeholder="Password"
-                                    required>
-                                <input v-model="confirmReg" type="password" class="form-control"
-                                    placeholder="Confirm Password" required>
-                                <input type="submit" class="btn btn-primary" @click="doRegister">
-                                <p>Already have an account? <a href="#"
-                                        @click="state.registerActive = !state.registerActive, emptyFields = false">Sign in here</a>
-                                </p>
-                            </form>
-                        </div>
+        <div class="container">
+            <div class="tw-grid lg:tw-grid-cols-2  tw-gap-4">
+                <div class=" tw-col-span-2 tw-my-10 tw-mx-auto">
+                    <div v-if="!state.registerActive" class="card login">
+                        <h1>Sign In</h1>
+                        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+                            <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+                                :rules="[val => val && val.length > 0 || 'Please type something']" />
+                            <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+                                :rules="[val => val && val.length > 0 || 'Please type something']" />
+                            <q-btn label="Submit" type="submit" color="primary" />
+                            <p>Don't have an account? <a href="#"
+                                    @click="state.registerActive = !state.registerActive, emptyFields = false">Sign up
+                                    here</a>
+                            </p>
+                            <p><a href="#">Forgot your password?</a></p>
+                        </q-form>
+                    </div>
+
+                    <div v-else class="card register">
+                        <h1>Sign Up</h1>
+                        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+                            <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+                                :rules="[val => val && val.length > 0 || 'Please type something']" />
+                            <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+                                :rules="[val => val && val.length > 0 || 'Please type something']" />
+                            <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+                                :rules="[val => val && val.length > 0 || 'Please type something']" />
+                            <q-btn label="Submit" type="submit" color="primary" />
+                            <p>Already have an account?
+                                <a href="#" @click="state.registerActive = !state.registerActive, emptyFields = false">
+                                    Sign in here
+                                </a>
+
+                            </p>
+                        </q-form>
                     </div>
                 </div>
-
             </div>
+
         </div>
+
 
     </div>
 </template>
